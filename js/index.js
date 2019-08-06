f30f4c772dd485769d7a4e1a2b6106b47c60bfe2
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-05 19:31:09 -0700

    less compiler is on

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let img_focus = document.querySelectorAll('img');
+
+img_focus.addEventListener('focusin', (event) => {
+    event.target.style.background = 'blue';    
+  });
+
